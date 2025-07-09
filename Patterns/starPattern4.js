85163d8cd51f8f698495cf8b9754fdda78d8d6ab
SinghJigyasa <jig86026@gmail.com>
2025-07-10 01:31:17 +0530

    Added diamond star pattern problem

--- Patterns/starPattern4.js
+++ Patterns/starPattern4.js
@@ -150,5 +150,39 @@ function getButterflyPattern(n) {
     console.log();
   }
 }
-getButterflyPattern(4)
+// getButterflyPattern(4)
+
+{
+  /**
+        *
+      *  *
+    *  *  *
+  *  *  *  *
+  *  *  *  *
+    *  *  *
+     *  *
+      *
+   */
+}
+function getDiamondStartPattern(n) {
+  for (let i = 1; i <= n; i++){
+    for (let j = 1; j <= n - i; j++){
+      process.stdout.write(" ")
+    }
+    for (let k = 1; k <= i; k++){
+      process.stdout.write(" * ")
+    }
+    console.log()
+  }
+  for (let i = n; i >0; i--) {
+    for (let j = 1; j <= n - i; j++) {
+      process.stdout.write(" ");
+    }
+    for (let k = 1; k <= i; k++) {
+      process.stdout.write(" * ");
+    }
+    console.log();
+  }
+}
+getDiamondStartPattern(10)
 
