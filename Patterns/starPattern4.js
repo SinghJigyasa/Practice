551b82e0a1dd3bf78e01d49245370f00936bab17
SinghJigyasa <jig86026@gmail.com>
2025-07-10 01:18:35 +0530

    Added advance star pattern problem

--- Patterns/starPattern4.js
+++ Patterns/starPattern4.js
@@ -60,5 +60,95 @@ function PrintStarReverse(n) {
     console.log()
   }
 }
-PrintStarReverse(5)
+// PrintStarReverse(5)
+
+{
+  /**
+   ********
+   ***  ***
+   **    **
+   *      *
+   *      *
+   **    **
+   ***  ***
+   ********
+   */
+}
+function getStarSquarePattern(n) {
+  //Decrement order loop
+  for (let i = n; i > 0; i--) {
+
+    //Print star (*)
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+
+    //Print space(" ")
+    for (let k = 1; k <= 2 * n - 2 * i; k++) {
+      process.stdout.write(" ");
+    }
+
+    //Print star(*)
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+    console.log();
+  }
+  //Increment order loop
+  for (let i = 1; i <= n; i++) {
+    //Print star
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+    //Print space
+    for (let k = 1; k <= 2 * n - 2 * i; k++) {
+      process.stdout.write(" ");
+    }
+    //Print star
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+    console.log();
+  }
+}
+// getStarSquarePattern(6);
+
+{
+  /**
+   *      *
+   **    **
+   ***  ***
+   ********
+   ***  ***
+   **    **
+   *      *
+   */
+}
+function getButterflyPattern(n) {
+  for (let i = 1; i <= n; i++){
+    for (let j = 1; j <=  i; j++){
+      process.stdout.write("*")
+    }
+    for (let k = 1; k <= 2 * n - 2 * i; k++){
+      process.stdout.write(" ")
+    }
+    for (let j = 1; j <= i; j++){
+      process.stdout.write("*")
+    }
+    console.log()
+  }
+  for (let i = n-1; i >0; i--) {
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+    for (let k = 1; k <= 2 * n - 2 * i; k++) {
+      process.stdout.write(" ");
+    }
+    for (let j = 1; j <= i; j++) {
+      process.stdout.write("*");
+    }
+    console.log();
+  }
+}
+getButterflyPattern(4)
 
